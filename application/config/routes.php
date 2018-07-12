a16efff25a9226bf2689d673c70b0e28ce2f69fe
Riza Masta Sputra <rizamasta@gmail.com>
2018-07-12 14:10:26 +0700

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,8 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route = array(
     'default_controller' => "Default/MyController/index",
-
-    'oky' => "User/User/index",
     '404_override' => 'Custom/error_404',
     'translate_uri_dashes' => TRUE
 );
